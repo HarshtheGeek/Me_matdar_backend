6666f9a16c3ce2d54e96fe715a2eb9aaa2686c99
Harsh <vishwaharsh10@gmail.com>
2025-11-01 02:45:01 +0530

    backend

--- Config/googleConfig.js
+++ Config/googleConfig.js
@@ -5,18 +5,18 @@ const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
 const getGoogleAuth = () => {
   try {
     const credentials = {
-      type: process.env.TYPE,
-      project_id: process.env.PROJECT_ID,
-      private_key_id: process.env.PRIVATE_KEY_ID,
+      type: process.env.type,
+      project_id: process.env.project_id,
+      private_key_id: process.env.private_key_id,
       // convert \n in private key to actual newlines
-      private_key: process.env.PRIVATE_KEY?.replace(/\\n/g, '\n'),
-      client_email: process.env.CLIENT_EMAIL,
-      client_id: process.env.CLIENT_ID,
-      auth_uri: process.env.AUTH_URI,
-      token_uri: process.env.TOKEN_URI,
-      auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
-      client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
-      universe_domain: process.env.UNIVERSE_DOMAIN,
+      private_key: process.env.private_key?.replace(/\\n/g, '\n'),
+      client_email: process.env.client_email,
+      client_id: process.env.client_id,
+      auth_uri: process.env.auth_uri,
+      token_uri: process.env.token_uri,
+      auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+      client_x509_cert_url: process.env.client_x509_cert_url,
+      universe_domain: process.env.universe_domain,
     };
 
     return new google.auth.GoogleAuth({
